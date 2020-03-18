39531bb045d5321749dbf6713358610f1e54b44f
Evan L Gilbert <Evan.Gilbert1212@gmail.com>
2020-03-18 16:12:57 -0400

    Connected to Octodex API

--- src/components/MyOctocats.js
+++ src/components/MyOctocats.js
@@ -1,42 +1,58 @@
 import React from 'react'
-import catstello from '../Images/catstello.png'
-import daftpunktocat_guy from '../Images/daftpunktocat-guy.gif'
-import daftpunktocat_thomas from '../Images/daftpunktocat-thomas.gif'
-import dunetocat from '../Images/Dunetocat.png'
-import fintechtocat from '../Images/Fintechtocat.png'
-import hulatocat from '../Images/Hulatocat.gif'
-import ironcat from '../Images/ironcat.jpg'
-import justicetocat from '../Images/justicetocat.jpg'
-import octobiwan from '../Images/octobiwan.jpg'
-import sentrytocat from '../Images/Sentrytocat.jpg'
-import spidertocat from '../Images/spidertocat.png'
-import stormtroopocat from '../Images/stormtroopocat.png'
 import Octocat from './Octocat'
 
 export class MyOctocats extends React.Component {
-  render() {
-    const myOctocats = [
-      { name: 'Fintechtocat', id: 149, url: fintechtocat },
-      { name: 'Sentrytocat', id: 143, url: sentrytocat },
-      { name: 'Hulatocat', id: 139, url: hulatocat },
-      { name: 'Justicetocat', id: 134, url: justicetocat },
-      { name: 'Catstello', id: 128, url: catstello },
-      { name: 'Dunetocat', id: 104, url: dunetocat },
-      { name: 'Daftpunktocat-Thomas', id: 101, url: daftpunktocat_thomas },
-      { name: 'Daftpunktocat-Guy', id: 100, url: daftpunktocat_guy },
-      { name: 'Spidertocat', id: 88, url: spidertocat },
-      { name: 'Stormtroopertocat', id: 85, url: stormtroopocat },
-      { name: 'Ironcat', id: 24, url: ironcat },
-      { name: 'Octobi Wan Catnobi', id: 3, url: octobiwan },
-    ]
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      myOctocats: [],
+    }
+  }
 
-    return (
-      <section>
-        {myOctocats.map(cat => {
-          return <Octocat name={cat.name} id={cat.id} url={cat.url} />
-        })}
-      </section>
-    )
+  componentDidMount() {
+    fetch('https://sdg-octodex.herokuapp.com/')
+      .then(result => result.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            myOctocats: result.data,
+          })
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error,
+          })
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, myOctocats } = this.state
+
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <section>
+          {myOctocats.map(cat => {
+            return (
+              <Octocat
+                name={cat.name}
+                id={cat.number}
+                image={cat.image}
+                authimage={cat.authors[0].image}
+              />
+            )
+          })}
+        </section>
+      )
+    }
   }
 }
 
--- src/components/Octocat.js
+++ src/components/Octocat.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
-import penguin25 from '../Images/penguin25.png'
 
 export class Octocat extends Component {
   render() {
     return (
       <article>
-        <img class="article-pic" src={this.props.url} alt={this.props.name} />
+        <img class="article-pic" src={this.props.image} alt={this.props.name} />
         <div class="article-num">#{this.props.id}:</div>
         <div class="article-authname">{this.props.name}</div>
         <img
           class="article-authimage"
-          src={penguin25}
-          alt="Penguin Profile Icon"
+          src={this.props.authimage}
+          alt="Author Icon"
         />
       </article>
     )
